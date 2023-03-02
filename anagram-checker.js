237758d988b05abb22a6b64bd4ba339f35b1c94a
Abenezer Assefa <94551524+AbenezerAssefa@users.noreply.github.com>
2023-03-02 17:55:11 +0300

    adding new files

---
+++ anagram-checker.js
@@ -0,0 +1,9 @@
+module.exports = function (str1, str2) {
+
+str1 = str1.replace(/[^0-9a-z]/gi, '').toLowerCase().split('').sort().join('');
+str2 =  str2.replace(/[^0-9a-z]/gi, '').toLowerCase().split('').sort().join('');
+  
+return str1 === str2
+
+  
+}
