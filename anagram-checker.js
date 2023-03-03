870721c8ba4858c08ed81c7d9d67e7f9cce83ad8
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-03-03 00:20:44 -0800

    Add JS file

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,9 +1,11 @@
 module.exports = function (str1, str2) {
-
-str1 = str1.replace(/[^0-9a-z]/gi, '').toLowerCase().split('').sort().join('');
-str2 =  str2.replace(/[^0-9a-z]/gi, '').toLowerCase().split('').sort().join('');
+    // write your code here
   
-return str1 === str2
-
   
-}
+  str1 = str1.replace(/[^0-9a-z]/gi, '').toLowerCase().split('').sort().join('');
+  str2 =  str2.replace(/[^0-9a-z]/gi, '').toLowerCase().split('').sort().join('');
+    
+  return str1 === str2
+  
+    
+  }
